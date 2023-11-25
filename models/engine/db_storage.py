6b00bd0e556697e8e5ae20f9163dd071b1dc5033
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-11-25 12:50:39 +0100

    added Review class to the list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,7 +37,7 @@ class DBStorage:
         '''
         db_dict = {}
 
-        classes = [State, City, User, Place]
+        classes = [State, City, User, Place, Review]
         if cls is None:
             for c in classes:
                 objs = self.__session.query(c).all()

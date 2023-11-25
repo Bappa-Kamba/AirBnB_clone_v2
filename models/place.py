8db2387dd8b3cc4670227d72ce7c3d28bb3272df
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-11-25 17:00:32 +0100

    b

--- models/place.py
+++ models/place.py
@@ -15,24 +15,6 @@ from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
-place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        primary_key=True,
-        nullable=False
-    ),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        primary_key=True,
-        nullable=False
-    )
-)
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -55,6 +37,25 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            primary_key=True,
+            nullable=False
+        ),
+        Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True,
+            nullable=False
+        )
+    )
 
     if storage_type == 'db':
 

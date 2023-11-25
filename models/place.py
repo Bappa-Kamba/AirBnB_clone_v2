297111a6b3729e1a8575ed9446f9351f84a26f8a
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-11-25 12:52:23 +0100

    resolved circular imports

--- models/place.py
+++ models/place.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import Place
 from sqlalchemy import (
     Column,
     String,

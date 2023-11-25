7b61b57e80a300698a43ae9f59376f516181e990
Bappa-Kamba <bappahmuhammad18@gmail.com>
2023-11-25 09:40:54 +0100

    tweaks

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -28,8 +28,8 @@ class DBStorage:
         if env == "test":
             # drop all tables
             Base.metadata.drop_all(self.__engine)
-        # self.__session = scoped_session(sessionmaker(
-        #     bind=self.__engine, expire_on_commit=False))
+        self.__session = scoped_session(sessionmaker(
+            bind=self.__engine, expire_on_commit=False))
         
     def all(self, cls=None):
         '''
@@ -69,11 +69,15 @@ class DBStorage:
         '''
         if obj is not None:
             self.__session.delete(obj)
+            self.save()
 
     def reload(self):
         '''
             Commit all changes of current database session
         '''
+        if self.__session:
+            self.__session.close()
+
         Base.metadata.create_all(self.__engine)
         factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(factory)

b0d8c5dedad572bd7245a49e0e0afa1fad523cc4
BappaKamba <bappahmuhammad18@gmail.com>
2024-01-01 05:34:48 -0800

    7-states_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web server that listens on 0.0.0.0:5000.
+
+Routes:
+    /states_list: Renders a template that displays the states present
+                    in the DBStorage sorted by name (`A-Z`)
+
+Usage:
+    Run this script to start the Flask web server.
+
+Example:
+    $ python3 7-states_list.py
+"""
+import os
+import sys
+
+# Get the parent directory path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Add the parent directory to the Python path
+sys.path.append(parent_dir)
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def clean_up(self):
+    """
+        removes the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+        display html page
+        fetch sorted states to insert into html in UL tag
+    """
+
+    states = [state for state in storage.all(State).values()]
+    sorted_states = sorted(states, key=lambda state: state.name)
+    print(sorted_states)
+    return render_template("7-states_list.html", states=sorted_states)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
